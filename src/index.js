baebb38d22fc691e865671bdaf2c578b3ca6593e
unknown <romyguibelondo@gmail.com>
2023-02-21 10:57:30 +0800

    fix css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '../../01tut/src/style/style.css'
+import './style/style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
